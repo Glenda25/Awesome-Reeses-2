508cfb1e0f27ebfb313e049c4107da77e80fb716
Glenda Mullan <glenda.mullan@gmail.com>
2022-10-08 15:39:43 +1100

    4. Finished

--- Reeses 2/ViewController.swift
+++ Reeses 2/ViewController.swift
@@ -22,10 +22,15 @@ class ViewController: UIViewController {
         let reesesMessage1 = "You Got Chocolate in my Peanut Butter!"
         let reesesMessage2 = "You got Peanut Butter in my Chocolate!"
         
+        //use label attributes bar to set line break to word wrap and set lines = 4
         if messageLabel.text == ""{
             messageLabel.text = reesesMessage1
+            messageLabel.textColor = .brown
+            messageLabel.textAlignment = .left
         }else if messageLabel.text == reesesMessage1 {
             messageLabel.text = reesesMessage2
+            messageLabel.textColor = .systemBrown
+            messageLabel.textAlignment = .right
         }else{
             messageLabel.text = ""
         }
